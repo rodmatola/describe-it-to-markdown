f0860bfba8f32bdf29323b79cb0f7238d54b9eb8
Rodrigo Matola <rodrigomatola@MacBook-Pro-de-Rodrigo.local>
2021-07-23 11:04:03 -0300

    first commit

---
+++ tomark.sh
@@ -0,0 +1,10 @@
+egrep -Rw 'describe|context|it' cypress/integration/front/ > cenarios.md
+sed 's/:/\n/g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+awk '!seen[$0]++' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/\t//g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/describe(/# Cenário: /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/context(/## **Contexto**: /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/it(/1. ## /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/it.only(/1. ## /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/it.skip(/1. ## (pendente)/g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/,.*$//' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
