1cb8addbfc152b252235a55ca64e4c7498bae603
Rodrigo Matola <rodmatola@gmail.com>
2021-09-21 23:22:59 -0300

    remove ' and add xit conversion

--- tomark.sh
+++ tomark.sh
@@ -1,4 +1,4 @@
-egrep -Rw 'describe|context|it' integration/ > cenarios.md
+egrep -Rw 'describe|context|it' integration > cenarios.md
 sed 's/:/\n/g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
 awk '!seen[$0]++' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
 sed 's/\t//g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
@@ -7,4 +7,6 @@ sed 's/context(/## **Contexto**: /g' cenarios.md > cenariostemp.md && mv cenario
 sed 's/it(/1. ## /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
 sed 's/it.only(/1. ## /g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
 sed 's/it.skip(/1. ## (pendente)/g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed 's/xit(/1. ## (pendente)/g' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
 sed 's/,.*$//' cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
+sed "s/'//g" cenarios.md > cenariostemp.md && mv cenariostemp.md cenarios.md
